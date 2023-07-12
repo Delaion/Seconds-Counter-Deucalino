ee76db18e7cd34d97e713b3d5e3494164bcd68dc
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-12 19:28:12 +0000

    need to redo the functions without window onload

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,27 @@
+import react from "react";
+import reactDom from "react-dom";
+import Home from "./home.jsx";
+var window.onload= function(){
+    let number=1
+    let counter=setInterval(function(){
+        number=number+1;
+    }, 1000); |
+    console.log(number);|
+    };
+const SecondsCounter =()=>{
+    return (
+        <div id="My_Seconds_Counter">
+            <div className="containerNumbers bg-dark">
+            <i className="fa-regular fa-clock-twelve"></i>
+            <p className="bg-dark-subtle">0</p>
+            <p className="bg-dark-subtle">0</p>
+            <p className="bg-dark-subtle">0</p>
+            <p className="bg-dark-subtle">0</p>
+            <p className="bg-dark-subtle" id="starter">{counter}</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default SecondsCounter;
